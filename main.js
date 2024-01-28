563ffc49babf72a23b7527f3c13f59ced7cd1d89
realest254 <petermainah160@gmail.com>
2024-01-28 12:54:42 +0300

    add code to change currentplayer status

--- main.js
+++ main.js
@@ -106,6 +106,7 @@ const MainGame = ((GameBoard,player1,player2) => {
             const moveResult = GameBoard.updateCell(row, column, currentPlayer);
             if (moveResult) {
                 cell.textContent = currentPlayer;
+                currPlayer.textContent = currentPlayer;
                 const winner = GameBoard.checkWinner();
                 spaces--;
                 if (!winner && spaces > 0) {
@@ -157,6 +158,43 @@ const MainGame = ((GameBoard,player1,player2) => {
             }
         }
     };
+    cells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+            const index = parseInt(cell.id);
+            let row, col;
+                
+            if (index === 1) {
+                row = 0;
+                col = 0;
+            } else if (index === 2) {
+                row = 0;
+                col = 1;
+            } else if (index === 3) {
+                row = 0;
+                col = 2;
+            } else if (index === 4) {
+                row = 1;
+                col = 0;
+            } else if (index === 5) {
+                row = 1;
+                col = 1;
+            } else if (index === 6) {
+                row = 1;
+                col = 2;
+            } else if (index === 7) {
+                row = 2;
+                col = 0;
+            } else if (index === 8) {
+                row = 2;
+                col = 1;
+            } else if (index === 9) {
+                row = 2;
+                col = 2;
+            } 
+            
+            makeMove(row, col, cell);
+        });
+    });
 
     function resetGame() {
         cells.forEach(cell => {
